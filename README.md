b5c71033ec88e58971929ad5b230fae80111d56b
WANGN0E <ning.wang.2@kaust.edu.sa>
2025-03-23 14:26:42 +0300

    change folder name

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
+Reproducible material for Geophysical Journal International - Wang N. and Ravasi M. "Imaging the Volve ocean-bottom field data with the Upside-down Rayleigh-Marchenko method"
+
 install_conda:
 	conda env create -f environment.yml && conda activate UD_RM 
 
+
+## Project structure
+This repository is organized as follows:
+
+* :open_file_folder: **udrm**: python library containing routines for upside-down RM method;
+* :open_file_folder: **dataset**: folder containing data 
+* :open_file_folder: **notebooks**: set of jupyter notebooks reproducing the experiments in the paper (see below for more details);
+
+
+## Notebooks
+The following notebooks are provided:
+
+- :orange_book: ``1project_wavefield_processed.ipynb``: notebook performing basic data processing for Volve field data;
+- :orange_book: ``2volve_sep_rec.ipynb``: notebook performing wavefield separation on the receiver side using PZ summation and source-deghosting;
+- :orange_book: ``3volve_sep_src_deghosting.ipynb``: notebook performing wavefield separation on the source side using source-deghosting;
+- :orange_book: ``4GF_one_point_modifyshift.ipynb``: notebook performing Green's function retrieval for Volve field data;
+- :orange_book: ``5raymckimaging_upd_volve.ipynb``: notebook performing Green's function retrieval for Volve field data in an imaging area;
+
+## Getting started :space_invader: :robot:
+To ensure reproducibility of the results, we suggest using the `environment.yml` file when creating an environment.
+
+Simply run:
+```
+./install_env.sh
+```
+It will take some time, if at the end you see the word `Done!` on your terminal you are ready to go. After that you can simply install your package:
+```
+pip install .
+```
+or in developer mode:
+```
+pip install -e .
+```
+
+Remember to always activate the environment by typing:
+```
+conda activate UD_RM
+```
 
---
+++ udrm/imaging.py
@@ -0,0 +1,248 @@
+import numpy as np
+import os
+import time
+import multiprocessing as mp
+
+from multiprocessing import set_start_method
+from multiprocessing import get_context
+from scipy.signal import convolve, filtfilt
+from pylops.waveeqprocessing.marchenko import directwave
+from pylops.waveeqprocessing.mdd import MDD
+from subroutine.marchenko import Marchenko
+from subroutine.raymarchenko import RayleighMarchenko
+from subroutine.anglegather import AngleGather
+from subroutine.raymarchenko_upd import RayleighMarchenko_upd
+from subroutine.raymarchenko_upd_vv import RayleighMarchenko_upd_vv
+
+def _imaging_depth_level(vsx, vsz, r, s, dr, ds, dt, nt, vel, 
+                         toff, nsmooth, wav, wav_c, niter, nfmax,
+                         igaths, nalpha,
+                         data, kind='mck', jt=1, ivsz=None, trav_s2=None, trav_mr2=None, verb=False):
+                         
+    print('ivsz = ',ivsz)
+    
+    if ivsz is not None and verb==True:
+        print('Working on depth level %d' % ivsz)
+
+    nr, ns = r.shape[1], s.shape[1]
+    nvsx = len(vsx)
+    dvsx = vsx[1] - vsx[0]
+    ngath = len(igaths)
+
+    if kind in ['mck', 'nmck']:
+        directVS = np.sqrt((vsx - r[0][:, np.newaxis]) ** 2 +
+                           (vsz - r[1][:, np.newaxis]) ** 2) / vel
+
+        G0 = np.zeros((nr, nvsx, nt))
+        for ivs in range(nvsx):
+            G0[:, ivs] = directwave(wav, directVS[:, ivs], nt, dt,
+                                    nfft=int(2 ** (np.ceil(np.log2(nt))))).T
+
+        mck = Marchenko(data['R'], dt=dt, dr=dr, nfmax=nfmax, wav=wav,
+                        toff=toff, nsmooth=nsmooth,
+                        saveRt=False, prescaled=False)
+        f1_inv_minus, f1_inv_plus, p0_minus, g_inv_minus, g_inv_plus = \
+            mck.apply_multiplepoints(directVS, nfft=2 ** 11, rtm=True,
+                                     greens=True, dottest=False,
+                                     **dict(iter_lim=niter,
+                                            show=False))
+
+    elif kind in ['rmck', ]:
+        directVSr = np.sqrt((vsx - r[0][:, np.newaxis]) ** 2 +
+                            (vsz - r[1][:, np.newaxis]) ** 2) / vel
+        directVSs = np.sqrt((vsx - s[0][:, np.newaxis]) ** 2 +
+                            (vsz - s[1][:, np.newaxis]) ** 2) / vel
+
+        G0rec = np.zeros((nr, nvsx, nt))
+        for ivs in range(nvsx):
+            G0rec[:, ivs] = directwave(wav, directVSr[:, ivs], nt, dt,
+                                       nfft=int(2 ** (np.ceil(np.log2(nt))))).T
+
+        G0 = np.zeros((ns, nvsx, nt))
+        for ivs in range(nvsx):
+            G0[:, ivs] = directwave(wav, directVSs[:, ivs], nt, dt,
+                                    nfft=int(2 ** (np.ceil(np.log2(nt))))).T
+
+        rm = RayleighMarchenko(data['Vzd'], data['Vzu'], dt=dt, dr=dr,
+                               nfmax=nfmax, wav=wav, toff=toff,
+                               nsmooth=nsmooth, saveVt=False, prescaled=False)
+        f1_inv_minus, f1_inv_plus, p0_minus, g_inv_minus, g_inv_plus = \
+            rm.apply_multiplepoints(directVSs, directVSr, G0=G0rec,
+                                    rtm=True, greens=True,
+                                    dottest=False,
+                                    **dict(iter_lim=niter,
+                                           show=False))
+    
+    elif kind in ['rmck_upd', ]:
+        directVSr = np.sqrt((vsx - r[0][:, np.newaxis]) ** 2 +
+                            (vsz - r[1][:, np.newaxis]) ** 2) / vel
+        directVSs = np.sqrt((vsx - s[0][:, np.newaxis]) ** 2 +
+                            (vsz - s[1][:, np.newaxis]) ** 2) / vel
+        FirstVSmr = np.sqrt((vsx - r[0][:, np.newaxis]) ** 2 +
+                            (vsz + r[1][:, np.newaxis]) ** 2) / vel
+
+        G0 = np.zeros((nr, nvsx, nt))
+        for ivs in range(nvsx):
+            G0[:, ivs] = directwave(wav, FirstVSmr[:, ivs], nt, dt,
+                                       nfft=int(2 ** (np.ceil(np.log2(nt))))).T
+
+        G0src = np.zeros((ns, nvsx, nt))
+        for ivs in range(nvsx):
+            G0src[:, ivs] = directwave(wav, directVSs[:, ivs], nt, dt,
+                                    nfft=int(2 ** (np.ceil(np.log2(nt))))).T
+
+        rm = RayleighMarchenko_upd(data['Vzd'], data['Vzu'], dt=dt, ds=ds,
+                                  nfmax=nfmax, wav=wav, toff=toff,
+                                  nsmooth=nsmooth, saveVt=False, prescaled=False)
+        f1_inv_minus, f1_inv_plus, p0_minus, g_inv_minus, g_inv_plus = \
+            rm.apply_multiplepoints_upd(directVSs, FirstVSmr, G0=G0src,
+                                       rtm=True, greens=True,
+                                       dottest=False,
+                                       **dict(iter_lim=niter,
+                                              show=False))
+
+    elif kind in ['rmck_upd_vv', ]:
+        directVSs =  trav_s2[:,ivsz,:].T
+        FirstVSmr =  trav_mr2[:,ivsz,:].T
+
+        G0 = np.zeros((nr, nvsx, nt))
+        for ivs in range(nvsx):
+            G0[:, ivs] = directwave(wav, FirstVSmr[:, ivs], nt, dt,
+                                       nfft=int(2 ** (np.ceil(np.log2(nt))))).T
+
+        G0src = np.zeros((ns, nvsx, nt))
+        for ivs in range(nvsx):
+            G0src[:, ivs] = directwave(wav, directVSs[:, ivs], nt, dt,
+                                    nfft=int(2 ** (np.ceil(np.log2(nt))))).T
+
+        rm = RayleighMarchenko_upd_vv(data['Vzd'], data['Vzu'], dt=dt, ds=ds,
+                                  nfmax=nfmax, wav=wav, toff=toff,
+                                  nsmooth=nsmooth, saveVt=False, prescaled=False)
+        _, _, p0_minus, g_inv_minus, g_inv_plus = \
+            rm.apply_multiplepoints_upd_vv(directVSs, FirstVSmr, G0=G0src,
+                                       rtm=True, greens=True,
+                                       dottest=False,
+                                       **dict(iter_lim=niter,
+                                              show=False))
+
+    # MDD
+    _, Rss = MDD(G0[:, :, ::jt], p0_minus[:, :, nt - 1:][:, :, ::jt],
+                 dt=jt * dt, dr=dvsx, twosided=True, adjoint=True, psf=False,
+                 wav=wav[wav_c - 60:wav_c + 60],
+                 nfmax=nfmax, dottest=False,
+                 **dict(iter_lim=0, show=0))
+
+    Rmck = MDD(g_inv_plus[:, :, nt - 1:][:, :, ::jt],
+               g_inv_minus[:, :, nt - 1:][:, :, ::jt],
+               dt=jt * dt, dr=dvsx, twosided=True, adjoint=False, psf=False,
+               wav=wav[wav_c - 60:wav_c + 60],
+               nfmax=nfmax,dottest=False,
+               **dict(iter_lim=10, show=0))
+
+    # Images
+    iss = np.diag(Rss[:, :, nt - 1])
+    imck = np.diag(Rmck[:, :, nt - 1])
+
+    # Angle gathers
+    ass = np.zeros((ngath, nalpha))
+    amck = np.zeros((ngath, nalpha))
+    for i, igath in enumerate(igaths):
+        ass[i], angle, Ra = AngleGather(Rss.transpose(2, 0, 1), nvsx, nalpha,
+                                        dt * jt, dvsx, igath, vel,
+                                        plotflag=False)
+        amck[i], angle, Ra = AngleGather(Rmck.transpose(2, 0, 1), nvsx, nalpha,
+                                         dt * jt, dvsx, igath, vel,
+                                         plotflag=False)
+    return iss, imck, ass, amck
+
+def MarchenkoImaging(vsx, vsz, r, s, dr, ds, dt, nt, vel,
+                     toff, nsmooth, wav, wav_c, nfmax, igaths, nalpha, jt,
+                     data, kind='rmck_upd', niter=10, nproc=1, nthreads=1, trav_s2=None, trav_mr2=None):
+    """Marchenko imaging
+
+    Perform one of Marchenko's redatuming techiniques and apply multi-dimensional
+    deconvolution to the retrieved Green's functions to obtain the local
+    reflection response. This routine can be run for multiple depth levels
+    and both images and angle-gathers can be produced as outputs
+
+    Parameters
+    kind=
+    'mck' or 'nmck': Marchenko method
+    
+    'rmck': Raylei Marchenko method
+    
+    'rmck_upd': Upside-down Raylei Marchenko method
+    
+    'rmck_upd_vv': Upside-down Raylei Marchenko method to deal with the variable velocity model
+    ----------
+    
+    Returns
+    -------
+    iss : :obj:`numpy.ndarray`
+        Single-scattering image
+    imck : :obj:`numpy.ndarray`
+        Marchenko image
+    ass : :obj:`numpy.ndarray`, optional
+        Single-scattering angle gathers
+    amck : :obj:`numpy.ndarray`, optional
+        Marchenko angle gathers
+    """
+    # Set threads
+    os.environ["OMP_NUM_THREADS"] = str(nthreads)
+    os.environ["MKL_NUM_THREADS"] = str(nthreads)
+
+    # Imaging loop (single processor)
+    nvsx, nvsz = len(vsx), len(vsz)
+
+    t0 = time.time()
+    if kind == 'rmck_upd_vv':
+    
+        if nproc == 1:
+            results = [_imaging_depth_level(vsx, vsz[ivsz], r, s, dr, ds, dt, nt, vel,
+                                            toff, nsmooth, wav, wav_c, niter, nfmax,
+                                            igaths, nalpha, data, kind=kind, jt=jt, ivsz=ivsz, 
+                                            trav_s2=trav_s2, trav_mr2=trav_mr2)
+                       for ivsz in range(nvsz)]
+        else:
+            print('nvsz = ',nvsz)
+            pool = mp.Pool(processes=nproc)
+            results = pool.starmap(_imaging_depth_level, [(vsx, vsz[ivsz], r, s, dr, ds, dt, nt, vel,
+                                                        toff, nsmooth, wav, wav_c, niter, nfmax,
+                                                        igaths, nalpha, data, kind, jt, ivsz, 
+                                                        trav_s2, trav_mr2) for ivsz in range(nvsz)])    
+        
+    else:
+        if nproc == 1:
+            results = [_imaging_depth_level(vsx, vsz[ivsz], r, s, dr, ds, dt, nt, vel,
+                                            toff, nsmooth, wav, wav_c, niter, nfmax,
+                                            igaths, nalpha, data,
+                                            kind=kind, jt=jt, ivsz=ivsz)
+                       for ivsz in range(nvsz)]
+        else:
+            print('nvsz = ',nvsz)
+        
+            pool = mp.Pool(processes=nproc)
+            results = pool.starmap(_imaging_depth_level, [(vsx, vsz[ivsz], r, s, dr, ds, dt, nt, vel, 
+                                         toff, nsmooth, wav, wav_c, niter, nfmax,
+                                         igaths, nalpha, data, kind, jt, ivsz) 
+                                         for ivsz in range(nvsz)])
+        
+    print('Elapsed time (mins): ', (time.time() - t0) / 60.)
+
+    iss = np.vstack([results[ivsz][0] for ivsz in range(nvsz)])
+    imck = np.vstack([results[ivsz][1] for ivsz in range(nvsz)])
+    ass = np.concatenate([results[ivsz][2][:, np.newaxis, :]
+                          for ivsz in range(nvsz)], axis=1)
+    amck = np.concatenate([results[ivsz][3][:, np.newaxis, :]
+                           for ivsz in range(nvsz)], axis=1)
+
+    #f1_inv_minus= np.concatenate([results[ivsz][4][:, np.newaxis, :] for ivsz in range(nvsz)], axis=1)
+    #f1_inv_plus= np.concatenate([results[ivsz][5][:, np.newaxis, :] for ivsz in range(nvsz)], axis=1)
+    #p0_minus= np.concatenate([results[ivsz][6][:, np.newaxis, :] for ivsz in range(nvsz)], axis=1)
+    #g_inv_minus= np.concatenate([results[ivsz][7][:, np.newaxis, :] for ivsz in range(nvsz)], axis=1)
+    #g_inv_plus= np.concatenate([results[ivsz][8][:, np.newaxis, :] for ivsz in range(nvsz)], axis=1)
+       
+    return iss, imck, ass, amck
+    
+    
+    
